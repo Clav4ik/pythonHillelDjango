9f8317a365cd1d221c4d7e428ccd6bcca48642ed
Clav4ik <89039462+Clav4ik@users.noreply.github.com>
2021-11-13 02:05:59 +0200

    Delete app directory

--- app/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    return HttpResponse("There is no text here<br><a href='home'>next page</a>")
-
-def home(request):
-    return HttpResponse("Hello world!")
