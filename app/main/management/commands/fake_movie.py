2c1645f9da18fd1ba210ca403be3f4eadaa1b0d7
Clav4ik <89039462+Clav4ik@users.noreply.github.com>
2021-11-29 16:52:30 +0200

    Complete hillel homework8

--- app/main/management/commands/fake_movie.py
+++ app/main/management/commands/fake_movie.py
@@ -2,35 +2,40 @@ from django.core.management import BaseCommand
 from faker import Faker
 from random import randrange, randint
 
-from main.models import Actor, Movie
+from main.models import Actor, Movie, Genre, Director
 
 
 class Command(BaseCommand):
-    help = "create fake movies with actors"
+    help = "create fake movies with actors, directors, genres existing in DB"
 
     def add_arguments(self, parser):
         parser.add_argument('-c', '--cmo', type=int, default=10)
 
     def handle(self, *args, **options):
 
-        genre_list = ["cartoon", "no cartoon"]
-        self.stdout.write("Start create movies with actors")
+        actors = Actor.objects.all()
+        directors = Director.objects.all()
+        genres = Genre.objects.all()
+        self.stdout.write("Start create movies with actors and other objects")
         print(options['cmo'])
         for _ in range(options['cmo']):
             faker = Faker()
             movie = Movie()
 
             movie.title = faker.sentence(nb_words=3)
-            movie.genre = faker.word(ext_word_list=genre_list)
-            movie.raiting = randint(-10, 10)
+            movie.data = faker.text()
+
+            movie.raiting = randint(-100, 100)
             movie.release_date = faker.date()
             movie.save()
-
-            for _ in range(randrange(1,4)):
-                actor = Actor()
-                actor.name = faker.name()
-                actor.save()
+            director = faker.word(ext_word_list=directors)
+            director.movie_set.add(movie)
+            for _ in range(randrange(1,5)):
+                actor = faker.word(ext_word_list=actors)
                 movie.actors.add(actor)
+            for _ in range(randrange(1,3)):
+                genre = faker.word(ext_word_list=genres)
+                movie.genre.add(genre)
 
             self.stdout.write(f'new movie\t{movie} {movie.genre}'
                               f' {movie.raiting} {movie.release_date} {movie.actors}')
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,85 @@
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-    return HttpResponse("There is no text here<br><a href='home'>next page</a>")
+from django.views.generic import TemplateView
 
-def home(request):
-    return HttpResponse("Hello world!")
+from .forms import MovieCreateForm
+from .models import *
+
+
+def index(request):
+    return HttpResponse("There is no text here<br><a href='movies'>next page</a>")
+
+
+class AllMovies(TemplateView):
+    template_name = 'main/index.html'
+
+    def get(self, request):
+        movies = Movie.objects.order_by('-id')
+        paginator = Paginator(movies, 20)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        error = ''
+        form = MovieCreateForm()
+        context = {
+            'form': form,
+            'error': error,
+            'page_obj': page_obj,
+            'title': 'All movies'
+        }
+        return render(request, 'main/index.html', context)
+    def post(self, request):
+        try:
+            dir = request.POST.get('director')
+            director = Director.objects.create(name=dir)
+            actor = Actor.objects.create(name=request.POST['actors'])
+            actor.save()
+            movie = Movie.objects.create(title=request.POST['title'],
+                                        data=request.POST['data'],
+                                        release_date=request.POST['release_date'],
+                                        director=director)
+
+            movie.save()
+            movie.actors.add(actor)
+            genre = Genre.objects.get(title=request.POST['genre'])
+            movie.genre.add(genre)
+            return redirect('home')  # переадресация на страничку home
+        except:
+            return self.get(request)
+
+
+
+def get_about_movie(request, movie_pk):
+    post_about_movie = get_object_or_404(Movie, pk=movie_pk)
+    # about = Movie_Genre.objects.all()
+    return render(request, 'main/about_movie.html', {'title': post_about_movie,
+                                                     "movie": post_about_movie})
+
+
+def get_top_movies(request):
+    movies = Movie.objects.order_by('-raiting')[:100]
+    context = {'title': 'Top movies',
+               "movies": movies}
+    return render(request, 'main/top_movies.html', context)
+
+
+def get_top_movies_int_value(request, int_value):
+    movies = Movie.objects.order_by('-raiting')[:int_value]
+    return render(request, 'main/top_movies.html', {'title': 'Top movies',
+                                               "movies": movies})
+
+
+class Vote(TemplateView):
+    template_name = 'main/vote.html'
+
+    def get(self, request,movie_pk):
+        return render(request, 'main/vote.html')
+
+    def post(self, request,movie_pk):
+        movie = Movie.objects.get(pk=movie_pk)
+        movie.raiting+= int(request.POST['fav_language'])
+        movie.save()
+        return redirect('about_movie',movie_pk)
