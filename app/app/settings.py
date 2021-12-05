68b5e0a5af73cedf4fc07c4df367e037261d7f08
Clav4ik <89039462+Clav4ik@users.noreply.github.com>
2021-12-05 22:20:06 +0200

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -57,8 +57,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'main.middletvar.RequestInfoTime'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'app.urls'
