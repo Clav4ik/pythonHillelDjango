685bef7c87c31e3eede75446627d9933b9b6b0e0
Clav4ik <89039462+Clav4ik@users.noreply.github.com>
2021-12-28 22:27:00 +0200

    add cache

--- app/main/urls.py
+++ app/main/urls.py
@@ -6,11 +6,11 @@ from django.views.decorators.cache import cache_page
 
 urlpatterns = [
 
-    path('', index, name='index'),
-    path('movies', AllMovies.as_view(), name='home'),
-    path('movies/<int:movie_pk>', get_about_movie, name='about_movie'),
-    path('movies/top', get_top_movies, name='top_movies'),
-    path('movies/top/<int:int_value>', get_top_movies_int_value,
+    path('', cache_page(60)(index), name='index'),
+    path('movies', cache_page(60)(AllMovies.as_view()), name='home'),
+    path('movies/<int:movie_pk>', cache_page(60)(get_about_movie), name='about_movie'),
+    path('movies/top', cache_page(60)(get_top_movies), name='top_movies'),
+    path('movies/top/<int:int_value>', cache_page(60)(get_top_movies_int_value),
          name='top_movies_int_value'),
     path('movies/<movie_pk>/rate', Vote.as_view(), name='vote')
 
